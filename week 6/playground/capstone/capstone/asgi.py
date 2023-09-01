a8309a21a1e291414bc36b4604360ffc873331f4
Krystle5183 <96323148+Krystle5183@users.noreply.github.com>
2023-09-01 15:12:47 -0500

    Create asgi.py

---
+++ week 6/playground/capstone/capstone/asgi.py
@@ -0,0 +1,10 @@
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'capstone.settings')
+
+application = get_asgi_application()
